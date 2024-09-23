d9ea292475191f63c08389bcffe5946d8844756e
ACrazyTown <47027981+ACrazyTown@users.noreply.github.com>
2024-09-23 16:42:31 +0200

    Update OpenAL Soft to 1.24.2

--- project/lib/custom/openal/build/version.h
+++ project/lib/custom/openal/build/version.h
@@ -1,8 +1,9 @@
 /* Define to the library version */
-#define ALSOFT_VERSION "1.20.1"
+#define ALSOFT_VERSION "1.24.2"
+#define ALSOFT_VERSION_NUM 1,24,2,0
 
 /* Define the branch being built */
-#define ALSOFT_GIT_BRANCH "master"
+#define ALSOFT_GIT_BRANCH "HEAD"
 
 /* Define the hash of the head commit */
-#define ALSOFT_GIT_COMMIT_HASH "f5e0eef3"
+#define ALSOFT_GIT_COMMIT_HASH "b621b5f"
