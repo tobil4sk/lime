61ec7d0913262a67eb577b85072f7cdff035ad03
Josh Tynjala <joshtynjala@bowlerhat.dev>
2024-11-20 09:35:42 -0800

    PlatformSetup: for lime setup hashlink, display the "leave empty to use default" message only when a custom version is not configured yet
    If a custom version is configured, users will think that just pressing enter will clear it, but it won't. It'll just keep the current value. To actually clear the value, they need to run lime config remove HL_PATH instead.

--- tools/utils/PlatformSetup.hx
+++ tools/utils/PlatformSetup.hx
@@ -1239,7 +1239,11 @@ class PlatformSetup
 
 	public static function setupHL():Void
 	{
-		getDefineValue("HL_PATH", "Absolute path to a custom version of HashLink. Leave empty to use Lime's default bundled version.");
+		var message = "Absolute path to a custom version of HashLink.";
+		if (ConfigHelper.getConfigValue("HL_PATH") == null) {
+			message += " Leave empty to use Lime's default bundled version.";
+		}
+		getDefineValue("HL_PATH", message);
 		if (System.hostPlatform == MAC)
 		{
 			Log.println("To use the HashLink debugger on macOS, the hl executable needs to be signed.");
